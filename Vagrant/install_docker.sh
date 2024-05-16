381296f4f7c99725971b98ab5276697909fd446f
willmiiller <will.miiller@hotmail.com>
2024-05-15 22:45:18 -0300

    vagrant

--- Vagrant/install_docker.sh
+++ Vagrant/install_docker.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 # Defina os servidores DNS desejados
 nameserver1="192.168.100.254"
-
 sudo dnf update
-sudo dnf install -y dnf-plugins-core
-sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 sudo dnf install docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
-sudo chmod 666 /var/run/docker.sock
+sudo chmod 777 /var/run/docker.sock
+
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+sudo setenforce 0
+sudo sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
+
 
 # Crie um novo resolv.conf com os servidores DNS desejados
 sudo bash -c "echo 'nameserver $nameserver1' > /etc/resolv.conf"
