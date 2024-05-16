5e21318c32e3971ca596f5c5e6aedc1df493e5f9
willmiiller <will.miiller@hotmail.com>
2024-05-15 22:56:43 -0300

    vagrant

--- Vagrant/install_docker.sh
+++ Vagrant/install_docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Defina os servidores DNS desejados
 nameserver1="192.168.100.254"
-sudo dnf update
+sudo dnf update -y
 sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io
+sudo dnf install docker-ce docker-ce-cli containerd.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
